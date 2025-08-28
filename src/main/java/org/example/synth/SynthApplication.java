70da0d6fc08e9a538fcd0047667358d49dd896e1
freddie_db <freddie1debruyn@gmail.com>
2025-08-28 16:13:30 +0100

    init

---
+++ src/main/java/org/example/synth/SynthApplication.java
@@ -0,0 +1,13 @@
+package org.example.synth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/synth/SynthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.synth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
